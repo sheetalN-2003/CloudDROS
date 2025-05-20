d15ea5d1994bf4fadf42b8b48b57646a95ff66b4
Sheetal N <157226830+sheetalN-2003@users.noreply.github.com>
2025-05-20 22:40:31 +0530

    Update streamlit_firebase.py

--- streamlit_firebase.py
+++ streamlit_firebase.py
@@ -1,1 +1,21 @@
+import pyrebase
 
+firebaseConfig = {
+  "apiKey": "YOUR_API_KEY",
+  "authDomain": "YOUR_PROJECT.firebaseapp.com",
+  "databaseURL": "",
+  "projectId": "YOUR_PROJECT",
+  "storageBucket": "YOUR_PROJECT.appspot.com",
+  "messagingSenderId": "...",
+  "appId": "..."
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+def login_user(email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
+    except:
+        return None
