ea1e11ba05e54f01269bc8e8a0cd99731b85c9ad
Sheetal N <157226830+sheetalN-2003@users.noreply.github.com>
2025-05-20 23:32:38 +0530

    Update streamlit_firebase.py

--- streamlit_firebase.py
+++ streamlit_firebase.py
@@ -4,7 +4,7 @@ import os
 firebaseConfig = {
   "apiKey": os.environ.get("FIREBASE_API_KEY"),
   "authDomain": os.environ.get("FIREBASE_PROJECT_ID") + ".firebaseapp.com",
-  "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
+  "projectId": os.environ.get("cloud-dros"),
   "storageBucket": os.environ.get("FIREBASE_PROJECT_ID") + ".appspot.com",
   "messagingSenderId": os.environ.get("FIREBASE_MESSAGING_SENDER_ID"),
   "appId": os.environ.get("FIREBASE_APP_ID")
